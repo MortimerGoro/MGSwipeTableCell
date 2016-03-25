3bf11f2f362848d91efac3ca0dc7c2fdb5e91439
Pavel Akhrameev <p.ahrameev@movavi.com>
2016-03-25 12:11:57 +0600

    Add allowSwipeDuringEditing flag to MGSwipeSetting

--- MGSwipeTableCell/MGSwipeTableCell.m
+++ MGSwipeTableCell/MGSwipeTableCell.m
@@ -849,7 +849,7 @@ static inline CGFloat mgEaseInOutBounce(CGFloat t, CGFloat b, CGFloat c) {
 -(void) setEditing:(BOOL)editing animated:(BOOL)animated
 {
     [super setEditing:editing animated:animated];
-    if (editing) { //disable swipe buttons when the user sets table editing mode
+    if (editing && self.cancelsSwipeOnEditing) { //disable swipe buttons when the user sets table editing mode
         self.swipeOffset = 0;
     }
 }
@@ -857,7 +857,7 @@ static inline CGFloat mgEaseInOutBounce(CGFloat t, CGFloat b, CGFloat c) {
 -(void) setEditing:(BOOL)editing
 {
     [super setEditing:YES];
-    if (editing) { //disable swipe buttons when the user sets table editing mode
+    if (editing && self.cancelsSwipeOnEditing) { //disable swipe buttons when the user sets table editing mode
         self.swipeOffset = 0;
     }
 }
@@ -956,6 +956,11 @@ static inline CGFloat mgEaseInOutBounce(CGFloat t, CGFloat b, CGFloat c) {
     if (_delegate && [_delegate respondsToSelector:@selector(swipeTableCell:didChangeSwipeState:gestureIsActive:)]) {
         [_delegate swipeTableCell:self didChangeSwipeState:_swipeState gestureIsActive: self.isSwipeGestureActive] ;
     }
+}
+
+- (BOOL) cancelsSwipeOnEditing
+{
+  return !self.leftSwipeSettings.allowSwipeDuringEditing && !self.rightSwipeSettings.allowSwipeDuringEditing;
 }
 
 #pragma mark Swipe Animation
@@ -1267,7 +1272,7 @@ static inline CGFloat mgEaseInOutBounce(CGFloat t, CGFloat b, CGFloat c) {
     
     if (gestureRecognizer == _panRecognizer) {
         
-        if (self.isEditing) {
+        if (self.isEditing && self.cancelsSwipeOnEditing) {
             return NO; //do not swipe while editing table
         }
         
